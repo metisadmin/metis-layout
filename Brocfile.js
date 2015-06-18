c67c2353029777df956d10dc949b75541aec514a
Osman Nuri Okumuş <onokumus@gmail.com>
2015-06-19 01:23:34 +0300

    v0.0.2

--- Brocfile.js
+++ Brocfile.js
@@ -5,24 +5,28 @@ var banner = '/**\n' +
   '* Homepage : ' + packageJson.homepage + '\n' +
   '* Author : ' + packageJson.author + '\n' +
   '* Copyright 2015\n' +
-  '* Licensed under ' + packageJson.license.type + ' (' + packageJson.license.url + ')\n' +
+  '* Licensed under ' + packageJson.license + '\n' +
   '*/\n';
 
 var concat = require('broccoli-concat');
 var funnel = require('broccoli-funnel');
 var compileLess = require('broccoli-less-single');
 var mergeTrees = require('broccoli-merge-trees');
+var uglifyJavaScript = require('broccoli-uglify-js');
 
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
-var autoprefixPlugin = new LessPluginAutoPrefix();
+var autoprefixPlugin = new LessPluginAutoPrefix({
+  browsers: ['last 2 versions', 'ie 10']
+});
 
+var bowerFiles = funnel('bower_components');
 var publicFolder = funnel('public');
 var projectFiles = funnel('app');
-var lessFiles = funnel(projectFiles, {
-  srcDir: 'styles'
-});
 var scriptFiles = funnel(projectFiles, {
   srcDir: 'scripts'
+});
+var lessFiles = funnel(projectFiles, {
+  srcDir: 'styles'
 });
 var less = compileLess(lessFiles, 'app.less', 'app.css', {
   plugins: [autoprefixPlugin]
@@ -31,16 +35,10 @@ var concatenatedLess = concat(less, {
   inputFiles: [
     'app.css'
   ],
-  outputFile: '/metis-layout.css',
+  outputFile: '/' + packageJson.name + '.css',
   header: banner
 });
-var concatenatedScripts = concat(scriptFiles, {
-  inputFiles: [
-    'app.js'
-  ],
-  outputFile: '/metis-layout.js',
-  header: banner
-});
+
 var lessMin = compileLess(lessFiles, 'app.less', 'app.min.css', {
   compress: true,
   plugins: [autoprefixPlugin]
@@ -49,8 +47,68 @@ var concatenatedLessMin = concat(lessMin, {
   inputFiles: [
     'app.min.css'
   ],
-  outputFile: '/metis-layout.min.css',
+  outputFile: '/' + packageJson.name + '.min.css',
   header: banner
 });
 
-module.exports = mergeTrees([publicFolder, concatenatedLess, concatenatedLessMin, concatenatedScripts]);
+var theme = compileLess(lessFiles, 'theme.less', 'theme.css', {
+  plugins: [autoprefixPlugin]
+});
+var themeConcatenatedLess = concat(theme, {
+  inputFiles: [
+    'theme.css'
+  ],
+  outputFile: '/' + packageJson.name + '-theme.css',
+  header: banner
+});
+
+var themeLessMin = compileLess(lessFiles, 'theme.less', 'theme.min.css', {
+  compress: true,
+  plugins: [autoprefixPlugin]
+});
+var themeConcatenatedLessMin = concat(themeLessMin, {
+  inputFiles: [
+    'theme.min.css'
+  ],
+  outputFile: '/' + packageJson.name + '-theme.min.css',
+  header: banner
+});
+
+var concatenatedScripts = concat(scriptFiles, {
+  inputFiles: [
+    'app.js'
+  ],
+  outputFile: '/' + packageJson.name + '.js',
+  header: banner
+});
+var concatenatedScriptsMin = concat(scriptFiles, {
+  inputFiles: [
+    'app.js'
+  ],
+  outputFile: '/' + packageJson.name + '.min.js',
+  header: banner
+});
+
+var uglifyScripts = uglifyJavaScript(concatenatedScriptsMin, {
+  compress: true,
+  mangle: true
+});
+
+var uglifyScriptsBanner = concat(uglifyScripts, {
+  inputFiles: [
+    packageJson.name + '.min.js'
+  ],
+  outputFile: '/' + packageJson.name + '.min.js',
+  header: banner
+});
+
+module.exports = mergeTrees([
+  bowerFiles,
+  publicFolder,
+  concatenatedLess,
+  concatenatedLessMin,
+  themeConcatenatedLess,
+  themeConcatenatedLessMin,
+  concatenatedScripts,
+  uglifyScriptsBanner
+]);
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,46 +1,5 @@
 (function(window) {
   'use strict';
 
-  var querySelector = document.querySelector.bind(document);
-
-  var sideBar = querySelector('.Sidebar');
-  var closeSideBtn = querySelector('.Sidebar-close');
-  var openSideBtn = querySelector('.Sidebar-open');
-  var toggleSideBtn = querySelector('.Sidebar-toggle');
-  var resizeTimer;
-
-  function closeSide() {
-    sideBar.classList.add('is-collapsed');
-  }
-
-  function openSide() {
-    sideBar.classList.remove('is-collapsed');
-  }
-
-  function toggleSide() {
-    sideBar.classList.toggle('is-collapsed');
-  }
-
-  closeSideBtn.addEventListener('click', closeSide);
-  openSideBtn.addEventListener('click', openSide);
-  toggleSideBtn.addEventListener('click', toggleSide);
-
-  function getViewPortWidth() {
-    var docElement = document.documentElement;
-    var client = docElement.clientWidth;
-    var inner = window.innerWidth;
-
-    return (client < inner) ? inner : client;
-  }
-
-  function respondSide() {
-    var viewPort = getViewPortWidth();
-    (viewPort < 768) ? closeSide() : openSide();
-  }
-
-  window.onload = respondSide();
-  window.onresize = function() {
-    window.clearTimeout(resizeTimer);
-    resizeTimer = window.setTimeout(respondSide, 100);
-  };
+  window.console.log('metis-layout loaded...');
 })(this);
